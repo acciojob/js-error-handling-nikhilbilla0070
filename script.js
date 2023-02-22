dc498679a53a4ef47b22c66b8b1b93d7239c271e
nikhilbilla0070 <114826561+nikhilbilla0070@users.noreply.github.com>
2023-02-22 23:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,56 +1,28 @@
 //your code here
-class OutOfRangeError extends Error {
-  constructor(arg) {
-    super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
-    this.name = "OutOfRangeError";
-  }
-}
+// Wait for the document to finish loading
+document.addEventListener("DOMContentLoaded", function() {
+  // Get references to the input field and the button
+  var inputField = document.getElementById("input1");
+  var button = document.getElementById("calculateButton");
 
-class InvalidExprError extends Error {
-  constructor() {
-    super("Expression should not have an invalid combination of expression");
-    this.name = "InvalidExprError";
-  }
-}
+  // Add an event listener to the button
+  button.addEventListener("click", function() {
+    // Get the value of the input field
+    var inputValue = inputField.value;
 
-function evalString(str) {
-  try {
-    // Check if expression starts with invalid operator
-    if (/^[+/*]/.test(str)) {
-      throw new SyntaxError("Expression should not start with invalid operator");
+    try {
+      // Evaluate the input value as a mathematical expression
+      var result = eval(inputValue);
+
+      // Update the result element with the calculated result
+      var resultElement = document.getElementById("result");
+      resultElement.innerHTML = "Result: " + result;
+    } catch (error) {
+      // Update the error element with the error message
+      var errorElement = document.getElementById("error");
+      errorElement.innerHTML = "Error: " + error.message;
     }
+  });
+});
 
-    // Check if expression ends with invalid operator
-    if (/[+/*-]$/.test(str)) {
-      throw new SyntaxError("Expression should not end with invalid operator");
-    }
-
-    // Check for invalid combination of operators
-    if (/([+/*-])\1/.test(str)) {
-      throw new InvalidExprError();
-    }
-
-    // Check if expression contains only valid characters
-    if (!/^[\d+\-*/\s]+$/.test(str)) {
-      throw new OutOfRangeError(str);
-    }
-
-    // Evaluate the expression and return the result
-    return eval(str);
-  } catch (err) {
-    // Handle errors
-    if (err instanceof OutOfRangeError || err instanceof InvalidExprError) {
-      console.error(`${err.name}: ${err.message}`);
-    } else {
-      throw err;
-    }
-  }
-}
-
-// Example usage
-try {
-  const result = evalString("1 + 2 * 3 - 4 / 2");
-  console.log("Result:", result);
-} catch (err) {
-  console.error(err);
-}
+   
